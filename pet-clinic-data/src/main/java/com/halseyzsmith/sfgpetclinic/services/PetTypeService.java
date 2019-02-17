147e4231de819979707d857d0fb7cdeeb74e08b6
Halsey Smith <halseyzsmith@gmail.com>
2019-02-17 13:55:40 -0500

    Section 7 Lecture 128 - Create PetType Map Service, Closes #29

---
+++ pet-clinic-data/src/main/java/com/halseyzsmith/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.halseyzsmith.sfgpetclinic.services;
+
+import com.halseyzsmith.sfgpetclinic.model.PetType;
+import com.halseyzsmith.sfgpetclinic.services.CrudService;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/halseyzsmith/sfgpetclinic/services/map/PetTypeMap.java
@@ -0,0 +1,35 @@
+package com.halseyzsmith.sfgpetclinic.services.map;
+
+import com.halseyzsmith.sfgpetclinic.model.PetType;
+import com.halseyzsmith.sfgpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        super.delete(petType);
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return super.save(petType);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
