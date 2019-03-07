58562ce75e3d69975a04f437c8a0f7c504671b43
Halsey Smith <halseyzsmith@gmail.com>
2019-03-06 19:57:53 -0500

    Section 8 Lecture 159 - Actuall add Spring Data to Pet Type Service

--- pet-clinic-data/src/main/java/com/halseyzsmith/sfgpetclinic/services/springdatajpa/PetTypeSDJpaService.java
+++ pet-clinic-data/src/main/java/com/halseyzsmith/sfgpetclinic/services/springdatajpa/PetTypeSDJpaService.java
@@ -1,4 +1,48 @@
 package com.halseyzsmith.sfgpetclinic.services.springdatajpa;
 
-public class PetTypeSDJpaService {
+import com.halseyzsmith.sfgpetclinic.model.PetType;
+import com.halseyzsmith.sfgpetclinic.repositories.PetTypeRepository;
+import com.halseyzsmith.sfgpetclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetTypeSDJpaService implements PetTypeService {
+
+    private PetTypeRepository petTypeRepository;
+
+    public PetTypeSDJpaService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> petTypes = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypes::add);
+        return petTypes;
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return petTypeRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return petTypeRepository.save(petType);
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        petTypeRepository.delete(petType);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petTypeRepository.deleteById(id);
+    }
 }
