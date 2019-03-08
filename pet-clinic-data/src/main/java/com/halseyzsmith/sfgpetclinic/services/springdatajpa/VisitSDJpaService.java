599999b1a68b38f848d6af4a99243b782c8f3cda
Halsey Smith <halseyzsmith@gmail.com>
2019-03-08 16:23:33 -0500

    Section 8 Lecture 164 - Add Spring Data JPA to Visit Service, closes #47.

---
+++ pet-clinic-data/src/main/java/com/halseyzsmith/sfgpetclinic/services/springdatajpa/VisitSDJpaService.java
@@ -0,0 +1,48 @@
+package com.halseyzsmith.sfgpetclinic.services.springdatajpa;
+
+import com.halseyzsmith.sfgpetclinic.model.Visit;
+import com.halseyzsmith.sfgpetclinic.repositories.VisitRepository;
+import com.halseyzsmith.sfgpetclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VisitSDJpaService implements VisitService {
+
+    VisitRepository visitRepository;
+
+    public VisitSDJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return visitRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        return visitRepository.save(visit);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        visitRepository.delete(visit);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        visitRepository.deleteById(id);
+    }
+}
