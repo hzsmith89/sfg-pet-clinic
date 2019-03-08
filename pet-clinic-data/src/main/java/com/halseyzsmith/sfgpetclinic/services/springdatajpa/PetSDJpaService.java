981216278c9c550916bda45776b73d98408387c2
Halsey Smith <halseyzsmith@gmail.com>
2019-03-08 12:10:20 -0500

    Section 8 Lecture 160 - Add Spring Data JPA to Pet Service, closes #43

---
+++ pet-clinic-data/src/main/java/com/halseyzsmith/sfgpetclinic/services/springdatajpa/PetSDJpaService.java
@@ -0,0 +1,48 @@
+package com.halseyzsmith.sfgpetclinic.services.springdatajpa;
+
+import com.halseyzsmith.sfgpetclinic.model.Pet;
+import com.halseyzsmith.sfgpetclinic.repositories.PetRepository;
+import com.halseyzsmith.sfgpetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetSDJpaService implements PetService {
+
+    private PetRepository petRepository;
+
+    public PetSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return petRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return petRepository.save(pet);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        petRepository.delete(pet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petRepository.deleteById(id);
+    }
+}
