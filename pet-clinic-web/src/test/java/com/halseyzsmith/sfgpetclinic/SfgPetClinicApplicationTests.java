803bcc2b65707bc0c9841c1cce0b1593441b944b
Halsey Smith <halseyzsmith@gmail.com>
2019-03-28 00:18:58 -0400

    Section 10 Lecture 189 - Upgrade to JUnit 5, Closes #4

--- pet-clinic-web/src/test/java/com/halseyzsmith/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/com/halseyzsmith/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,12 +1,12 @@
 package com.halseyzsmith.sfgpetclinic;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
 public class SfgPetClinicApplicationTests {
 
     @Test
@@ -14,3 +14,5 @@ public class SfgPetClinicApplicationTests {
     }
 
 }
+
+
