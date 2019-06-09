442d8a3a1e7800038ee3b7ae5cf6465d5909ffa8
Halsey Smith <halseyzsmith@gmail.com>
2019-06-09 17:11:45 -0400

    Testing ClientCI failed test

--- pet-clinic-web/src/test/java/com/halseyzsmith/sfgpetclinic/services/map/OwnerMapServiceTest.java
+++ pet-clinic-web/src/test/java/com/halseyzsmith/sfgpetclinic/services/map/OwnerMapServiceTest.java
@@ -28,7 +28,7 @@ class OwnerMapServiceTest {
     void findAllTest() {
         Set<Owner> owners = ownerMapService.findAll();
 
-        assertEquals(1, owners.size());
+        assertEquals(2, owners.size());
     }
 
     @Test
